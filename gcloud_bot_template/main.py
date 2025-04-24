9dcbc1bf0e46b647437925ee8766d2c0663ca456
PN-works <a93641192@gmail.com>
2025-04-24 13:38:39 +0900

    Update main.py

--- gcloud_bot_template/main.py
+++ gcloud_bot_template/main.py
@@ -15,6 +15,25 @@ async def 핑(ctx):
     await ctx.send("퐁!")
 
 TOKEN = os.getenv("DISCORD_TOKEN")
+
+from flask import Flask
+from threading import Thread
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "봇 작동 중!"
+
+def run():
+    port = int(os.environ.get("PORT", 8080))  # Cloud Run에서 반드시 필요
+    app.run(host="0.0.0.0", port=port)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.daemon = True
+    t.start()
+    
 if __name__ == "__main__":
     keep_alive()
     bot.run(TOKEN)
